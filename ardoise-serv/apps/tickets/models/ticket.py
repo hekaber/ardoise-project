3f65c3cef2b4cc2e26f3c3d9fc497464e66dccbe
hkb <hkb@tisynch.com>
2020-10-14 21:27:40 +0200

    post + get contact

--- ardoise-serv/apps/tickets/models/ticket.py
+++ ardoise-serv/apps/tickets/models/ticket.py
@@ -8,7 +8,7 @@ class Ticket(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(Contact, related_name='%(class)s_related_owner', on_delete=models.CASCADE)
     debtor = models.ForeignKey(Contact, related_name='%(class)s_related_debtor', null=True, on_delete=models.SET_NULL)
-    amount = models.IntegerField(min_value=0)
+    amount = models.IntegerField()
 
     class Meta:
         db_table = 'ticket'
