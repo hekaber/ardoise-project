d6d224da3d3097e81f93dda02af7b4b30b8e5572
hkb <hkb@tisynch.com>
2020-10-31 12:56:05 +0100

    improve invitations list

--- ardoise-serv/apps/contacts/views/invite.py
+++ ardoise-serv/apps/contacts/views/invite.py
@@ -2,6 +2,7 @@ from apps.contacts.models import Contact, Invite, DEFAULT_INVITE_ID, INVITE_STAT
 from apps.shared.models import Status
 from apps.contacts.forms import InviteForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import generic
 
@@ -10,36 +11,31 @@ class InviteView(LoginRequiredMixin, generic.TemplateView):
     login_url = '/accounts/login/'
     template_name = 'contacts/invite.html'
 
-    # def post(self, request, *args, **kwargs):
-    #     form = InviteForm(request.POST, contact_id=request.user.contact_id)
-    #     if form.is_valid():
-    #         invite = Invite()
-    #         invite.from_contact = request.user.contact_id
-    #         invite.to_contact = form.cleaned_data['owner']
-    #         invite.status = DEFAULT_INVITE_ID
-    #         invite.save()
-    #
-    #     return render(request, 'contacts/invite.html', {'form': form})
+    def get(self, request, *args, **kwargs):
 
-    def get_context_data(self, **kwargs):
+        search = self.request.GET.get('search', '')
+
+        queryset = Contact.objects.exclude(pk=self.request.user.contact_id)
+
+        if search:
+            queryset = Contact.objects.filter((
+                Q(first_name__contains=search) |
+                Q(last_name__contains=search) |
+                Q(email__contains=search)) &
+                ~Q(pk=self.request.user.contact_id)
+            )
+
         context = super().get_context_data(**kwargs)
 
         invites = Invite.objects.filter(
             from_contact=self.request.user.contact_id
         ).values(
             'to_contact',
-            'status_id'
+            'status_id',
+            'status__label'
         )
 
-        statuses = Status.objects.filter(
-            category=INVITE_STATUS_CATEGORY
-        ).values('id', 'label')
+        context['invites_map'] = {invite.get('to_contact'): invite.get('status__label') for invite in invites}
+        context['contact_list'] = queryset
 
-        statuses_dict = {status.get('id'): status.get('label') for status in statuses}
-        invite_dict = {}
-        for invite in invites:
-            invite_dict[invite.get('to_contact')] = statuses_dict.get(invite.get('status_id'))
-        context['current_invites'] = invite_dict.keys()
-        context['invites_map'] = invite_dict
-        context['contact_list'] = Contact.objects.all()
-        return context
+        return self.render_to_response(context)
--- ardoise-serv/config/settings.py
+++ ardoise-serv/config/settings.py
@@ -162,6 +162,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = '/ticket/list'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
